7034ff73e05b7182a0d4be709443c8afc08f3d9d
Peggy <Peggy@peggydemacbook-pro.local>
2016-04-19 19:46:58 +0800

    Initial Commit

---
+++ GuessANumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessANumber
+//
+//  Created by Peggy on 2016/4/19.
+//  Copyright © 2016年 Peggy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
